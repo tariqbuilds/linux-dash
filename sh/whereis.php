5a09aec712b5ed521dadc2623684e39ca79b459c
madebycm <christian.meinhold@gmail.com>
2014-01-28 10:14:41 +0100

    Add «node» to whereis

--- sh/whereis.php
+++ sh/whereis.php
@@ -1,6 +1,6 @@
 <?php 
 
-    exec('whereis php mysql vim python ruby java apache2 nginx openssl vsftpd make'.
+    exec('whereis php node mysql vim python ruby java apache2 nginx openssl vsftpd make'.
           '|awk \'{ split($1, a, ":");if (length($2)==0) print a[1]",Not Installed"; else print a[1]","$2;}\'',$result);
     
     
