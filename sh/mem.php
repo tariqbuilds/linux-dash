4b2d9fddf4b1d04431d58d70bd210d6f6f0c3c17
glasspelican <glasspelican@gmail.com>
2014-01-27 23:24:33 -0500

    Show memory used by applications, not used + disk cache

--- sh/mem.php
+++ sh/mem.php
@@ -2,5 +2,5 @@
 
     //exec('cat /proc/meminfo|awk \'{print $1","$2}\'',$result);
     //exec('free -m|awk \'{print $1","$2","$3","$4}\'',$result);
-    exec('free -tmo|awk \'{print $1","$2","$3","$4}\'',$result);
+    exec('free -tmo|awk \'{print $1","$2","$3-$6-$7","$4+$6+$7}\'',$result);
     echo json_encode( explode(',',$result[1]) );
