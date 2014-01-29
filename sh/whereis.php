f34c7b1b49044cc38b7de42bbb38a75ea0948c51
Josh Morningstar <j.morningstar@me.com>
2014-01-28 19:19:13 -0500

    Finalizing formating for whereis.php and top.php.  Also changed 'whereis' to which promotes cross-server usage of this script.

--- sh/whereis.php
+++ sh/whereis.php
@@ -1,17 +1,36 @@
 <?php 
 
-    exec('whereis php mysql vim python ruby java apache2 nginx openssl vsftpd make'.
-          '|awk \'{ split($1, a, ":");if (length($2)==0) print a[1]",Not Installed"; else print a[1]","$2;}\'',$result);
+/**
+ * Returns the path data for php, mysql, mysqld, python, vim, ruby, make, apache (2), nginx, openssl, vsftpd
+ *
+ * @author afaqurk <https://github.com/afaqurk>
+ * @author Josh Morningstar <rebl00ded@gmail.com>
+ *
+ * @package Linux Dashboard
+ */
+
+exec('which php mysql vim python ruby java apache2 nginx openssl vsftpd make'. '|awk \'{ split($1, a, ":");if (length($2)==0) print a[1]",Not Installed"; else print a[1]","$2;}\'', $resultss );
     
     
-    echo "[";
-    $x = 0;
-    $max = count($result)-1;
-    foreach ($result as $a)
-    {    
-        echo json_encode( explode(',',$result[$x]) );
-        echo ($x==$max)?'':',';
-        unset($result[$x],$a);
-        $x++;
-    }
-    echo ']';
+$jData	= "[";
+$x		= 0;
+$max	= count ( $results ) - 1;
+
+foreach ( $results as $result )
+{    
+	$jData .= json_encode ( explode ( ',', $results [ $x ] ) );
+	$jData .= ( $x == $max ) ? '' : ',';
+
+	unset ( $results [ $x ], $result );
+
+	$x ++;
+}
+
+$jData .= ']';
+
+echo $jData;
+
+/**
+ * End of file whereis.php
+ * Location ./sh/whereis.php
+ */
