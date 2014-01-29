e079c9b3785776b05ec3832e3d9185cbe6190e8b
Mikkel Kjeldsen <commonquail@gmail.com>
2014-01-30 00:43:48 +0100

    Cancel active pulse before starting new.

--- js/base.js
+++ js/base.js
@@ -11,20 +11,43 @@ $(document).ready(function() {
     });
 });
 
+// Handle for cancelling active effect.
+var pulsing = {
+    element: null,
+    timeoutIDs: [],
+    resetfn: function() {
+        pulsing.element = null;
+        pulsing.timeoutIDs = [];
+    }
+};
+
 /**
- * Applies a pulse effect to the specified element.
+ * Applies a pulse effect to the specified element. If triggered while already
+ * active the ongoing effect is cancelled immediately.
  *
  * @param {HTMLElement} element The element to apply the effect to.
  * @param {Number} times How many pulses.
  * @param {Number} interval Milliseconds between pulses.
  */
 function pulseElement(element, times, interval) {
+    if (pulsing.element) {
+        pulsing.element.removeClass('pulse').
+            parent().removeClass('pulse-border');
+        pulsing.timeoutIDs.forEach(function(ID) {
+            clearTimeout(ID);
+        });
+        pulsing.timeoutIDs = [];
+    }
+    pulsing.element = element;
     var parent = element.parent();
     var f = function() {
         element.toggleClass('pulse');
         parent.toggleClass('pulse-border');
     };
+
+    pulsing.timeoutIDs.push(setTimeout(pulsing.resetfn,
+                                       (times + 1) * interval));
     for (; times > 0; --times) {
-        setTimeout(f, times * interval);
+        pulsing.timeoutIDs.push(setTimeout(f, times * interval));
     }
 }
