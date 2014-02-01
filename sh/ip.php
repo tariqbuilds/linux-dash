ee25e12a559efd27ac1852f74dc961602bc627ec
Jan van Haarst <jan@vanhaarst.net>
2014-02-01 22:58:13 +0100

    Show IPv6 adresses, use "ip".
    Also return IPv6 adresses, use "ip" to get the information, but fallback to ifconfig if that fails.

--- sh/ip.php
+++ sh/ip.php
@@ -1,12 +1,23 @@
-<?php  
-    exec('/sbin/ifconfig |grep -B1 "inet addr" |awk \''.
-         '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:",$1 } }\' |awk'.
-         ' -F: \'{ print $1","$3 }\'',$result);
-    
+<?php
+    // First try to get the IPs using "ip"
+    // First get list of links
+    $command='ip -oneline link show | awk \'{print $2}\' | sed "s/://"';
+    exec($command,$result,$error);
+    if ( $error ) { // It didn't work with "ip" , so we do it with ifconfig
+	exec('/sbin/ifconfig |grep -B1 "inet addr" |awk \''.
+	    '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:",$1 } }\' |awk'.
+	    ' -F: \'{ print $1","$3 }\'',$result);
+    }else{
+	$result = implode(' ',$result);
+	// Now use that list to get the ip-adresses
+	$command = 'for interface in '.$result.';do for family in inet inet6;do ip -oneline -family $family addr show $interface | grep -v fe80 | awk \'{print $2","$4}\';done;done';
+        exec($command,$result,$return_value);
+    }
+    // Get external adress
     exec('curl http://ipecho.net/plain; echo',$result2);
-    
+    // Create JSON header
     header('Content-Type: application/json; charset=UTF-8');
-
+    // Return info as JSON
     echo '[[','"external ip","',$result2[0],'"]';
     $x = 0;
     $max = count($result)-1;
