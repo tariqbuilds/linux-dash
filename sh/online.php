742cc7260cb0aecd9f36efa252ffb825ee8cd1f4
dongweiming <ciici1234@hotmail.com>
2014-02-02 05:43:00 -0500

    Remove the first set of quotation

--- sh/online.php
+++ sh/online.php
@@ -2,7 +2,7 @@
 
     // change username column length for w command
     putenv("PROCPS_USERLEN=20");
-    exec('w -h | awk \'{print ""$1","$3","$4","$5}\'',$users);
+    exec('w -h | awk \'{print $1","$3","$4","$5}\'',$users);
     $result = array();
     foreach ($users as $user)
     {
