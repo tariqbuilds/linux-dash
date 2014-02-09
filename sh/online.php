43ce9042658d36d34e46549bddb7bb93489612ec
armandg <armandg@gmail.com>
2014-02-09 22:56:12 +0100

    Update online.php
    Added PROCPS_FROMLEN=40 to increase the width of the user-address. Testing showed that my address was a whopping 26 chars, but only showed 16 chars (which is standard with w according to man).

--- sh/online.php
+++ sh/online.php
@@ -2,7 +2,7 @@
 
     // change username column length for w command
     putenv("PROCPS_USERLEN=20");
-    exec('/usr/bin/w -h | /usr/bin/awk \'{print $1","$3","$4","$5}\'',$users);
+    exec('PROCPS_FROMLEN=40 /usr/bin/w -h | /usr/bin/awk \'{print $1","$3","$4","$5}\'',$users);
     $result = array();
     foreach ($users as $user)
     {
