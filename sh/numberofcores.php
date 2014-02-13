e9cd85dcdfa65901f5ae30e305b1df1f4e12efac
Afaq Tariq <afaq05@gmail.com>
2014-02-13 07:12:05 -0800

    added fallback for number of cores

--- sh/numberofcores.php
+++ sh/numberofcores.php
@@ -1,6 +1,18 @@
 <?php 
+    // Initialize cores to 0
+    $numOfCores = 0;
 
-    exec('/bin/grep -c ^processor /proc/cpuinfo',$result);
+    // get value via /proc/cpuinfo
+    $numOfCores = shell_exec('/bin/grep -c ^processor /proc/cpuinfo');
+    $numOfCores = $numOfCores[0];
+
+    // If number of cores is not found, run fallback
+    if( $numOfCores == 0 ){
+
+       // run nproc fallback command
+       $numOfCores = shell_exec('nproc');
+    }
+
     header('Content-Type: application/json; charset=UTF-8');
-    echo json_encode( $result[0] );
+    echo json_encode( $numOfCores );
     
