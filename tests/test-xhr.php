9629c97bda8b792f06f6ddbf8dc85a8b45828f06
qrz <eelggat@gmail.com>
2014-02-15 01:41:47 +1000

    Style Guidance

--- tests/test-xhr.php
+++ tests/test-xhr.php
@@ -3,13 +3,17 @@
     try
     {
         if (isset($_GET['sh']))
+        {
             include("sh/".$_GET['sh'].".php");
+        }
         else
+        {
             throw new HttpQueryStringException($_GET['sh']);
+        }
     }
     catch (Exception $e)
     {
-        echo json_encode((object)array(
+        echo json_encode(array(
             'error'=>$e->getMessage()
         ));
     }
