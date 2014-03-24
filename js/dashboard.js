c2d6cc954d66ab5c43a8ade53acc1068afd3328f
Krzysztof Suszyński <krzysztof.suszynski@coi.gov.pl>
2014-03-24 10:54:59 +0100

    Fixes for bandwidth

--- js/dashboard.js
+++ js/dashboard.js
@@ -448,6 +448,7 @@ dashboard.getBandwidth = function () {
     $.ajax({
         url: 'sh/bandwidth.php',
         cache: false,
+        dataType: 'json',
         success: function (data) {
             $('#bw-tx').text(data.tx);
             $('#bw-rx').text(data.rx);
