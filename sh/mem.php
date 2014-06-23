7298dae40275eae409194c994ef352f74c66250f
Jason Barnett <jason.w.barnett@gmail.com>
2014-06-23 11:19:09 -0700

    Added OFS to the awk command for simplicity and clarity sake.

--- sh/mem.php
+++ sh/mem.php
@@ -1,9 +1,9 @@
 <?php
 
-//exec('cat /proc/meminfo|awk \'{print $1","$2}\'',$result);
-//exec('free -m|awk \'{print $1","$2","$3","$4}\'',$result);
+//exec('cat /proc/meminfo | awk \'BEGIN {OFS=","} {print $1,$2}\'',$result);
+//exec('free -m | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4}\'',$result);
 exec(
-    '/usr/bin/free -tmo | /usr/bin/awk \'{print $1","$2","$3-$6-$7","$4+$6+$7}\'',
+    '/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3-$6-$7,$4+$6+$7}\'',
     $result
 );
 
