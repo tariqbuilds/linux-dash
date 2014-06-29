98cda25792681a8a2e8c501038e660e75c7aac06
Serafeim Papastefanos <spapas@gmail.com>
2014-06-29 23:46:50 +0300

    Fix arp-module behaviour
    Change the array initialization in the arp module from [] to array() to stop a 500 exception from appearing an php 5.3.
    
    Also, check if the arp executable is in /usr/sbin/arp and if not found fall back to /sbin/arp.

--- libs/Modules/arp.php
+++ libs/Modules/arp.php
@@ -7,9 +7,9 @@
 
         public function getData($args=array()) {
 
-	    exec('/usr/sbin/arp | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4,$5}\'', $result);
+	    exec('if [ -e /usr/sbin/arp ] ; then /usr/sbin/arp ; else /sbin/arp ; fi | awk \'BEGIN {OFS=","} {print $1,$2,$3,$4,$5}\'', $result);
 
-	    $data = [];
+	    $data = array();
 	    $x = 0;
 
 	    foreach ($result as $a) {
