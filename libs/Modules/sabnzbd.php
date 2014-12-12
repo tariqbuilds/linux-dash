9f072bffba9e5f67503e25ad6e50c013b96ecc1c
Afaq Tariq <afaq05@gmail.com>
2014-12-12 18:16:05 -0500

    Updated sabnzd module to supress and catch file reading warning from PHP when it is not installed

--- libs/Modules/sabnzbd.php
+++ libs/Modules/sabnzbd.php
@@ -11,7 +11,10 @@
             $API = getenv("SABNZBD_API");
             $URL = $HOST."/sabnzbd/api?mode=qstatus&output=json&apikey=".$API;
 
-            $json = file_get_contents($URL);
+            @$json = file_get_contents($URL);
+            
+            if(!$json) return '-';
+            
             $json_output = json_decode($json);
             $speedDownstream = preg_replace("/[^\.0-9]+/", "", $json_output->{'speed'});
             $speedDownstream = $speedDownstream * 1024;
