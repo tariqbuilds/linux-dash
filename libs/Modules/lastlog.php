aec06372fe1937610c542002dc574b4167e1aeba
Afaq Tariq <afaq05@gmail.com>
2014-12-15 07:57:49 -0500

    fixes #246. last login date now is in ISO format (yyyy-mm-dd h:m:s)

--- libs/Modules/lastlog.php
+++ libs/Modules/lastlog.php
@@ -12,10 +12,16 @@
                 ' /usr/bin/awk \'{print $1","$3","$4" "$5" "$6" "$7" "$8}\'',
                 $users
             );
+            
             $result = array();
+            $row = array();
+
             # ignore the first line of column names
             for ($i = 1; $i < count($users); ++$i) {
-                $result[$i-1] = explode(",", $users[$i]);
+                $row = explode(",", $users[$i]);
+                $row[2] = strtotime($row[2]);
+                $row[2] = date('Y-m-d H:i:s',$row[2]);
+                $result[$i-1] = $row;                       
             }
 
             return $result;
