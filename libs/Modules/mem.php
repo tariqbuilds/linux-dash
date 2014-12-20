19d2a38833c96e62a6add6514cf1d4176afecc88
chris laptop <christophersimonandrews@gmail.com>
2014-12-20 16:08:53 +0000

    Added support to the procps-ng version of the free utility as it has no -o switch.

--- libs/Modules/mem.php
+++ libs/Modules/mem.php
@@ -5,12 +5,15 @@
     class mem extends \ld\Modules\Module {
         protected $name = 'mem';
 
-        public function getData($args=array()) {
-            exec(
-                '/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3-$6-$7,$4+$6+$7}\'',
-                $result
-            );
+	public function getData($args=array()) {
+	    exec('/usr/bin/free -V | /usr/bin/grep procps-ng', $ng);
+	    if (empty($ng)) {
+	    	exec('/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3-$6-$7,$4+$6+$7}\'', $result);
 
+	    }
+	    else {
+            	exec('/usr/bin/free -tm | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3+$6,$4}\'', $result);
+	    }
             return explode(',', $result[1]);
         }
     }
