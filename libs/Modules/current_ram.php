07804fd31b4133ec5fce5d73f9b370a211ad3c08
Afaq Tariq <afaq05@gmail.com>
2015-01-25 15:17:37 -0500

    ram chart widget should now be working for all free versions

--- libs/Modules/current_ram.php
+++ libs/Modules/current_ram.php
@@ -8,7 +8,7 @@
 	public function getData($args=array()) {
 	    exec('/usr/bin/free -V | /bin/grep procps-ng', $ng);
 	    if (empty($ng)) {
-	    	exec('/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3-$6-$7,$4+$6+$7}\'', $result);
+	    	exec('/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} NR>1 {print $2,$3,$4}\'', $result);
 
 	    }
 	    else {
