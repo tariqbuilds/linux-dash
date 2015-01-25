438d5eb54a4043989c820060ebaaccc78522118c
Afaq Tariq <afaq05@gmail.com>
2015-01-25 14:47:14 -0500

    Fixes #274

--- libs/Modules/current_ram.php
+++ libs/Modules/current_ram.php
@@ -6,7 +6,7 @@
         protected $name = 'current_ram';
 
 	public function getData($args=array()) {
-	    exec('/usr/bin/free -V | /usr/bin/grep procps-ng', $ng);
+	    exec('/usr/bin/free -V | /bin/grep procps-ng', $ng);
 	    if (empty($ng)) {
 	    	exec('/usr/bin/free -tmo | /usr/bin/awk \'BEGIN {OFS=","} {print $1,$2,$3-$6-$7,$4+$6+$7}\'', $result);
 
