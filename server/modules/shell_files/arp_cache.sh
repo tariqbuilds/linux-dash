f13972ae2f94916537995dd34210e699950064ba
Afaq Tariq <afaq05@gmail.com>
2015-02-19 19:29:34 -0500

    arp module account for no results

--- server/modules/shell_files/arp_cache.sh
+++ server/modules/shell_files/arp_cache.sh
@@ -2,12 +2,16 @@
 
 arpCommand=$(command -v arp) 
 
-$arpCommand | awk 'BEGIN {print "["} NR>1 \
-						{print "{ \"address\": \"" $1 "\", " \ 
+result=$($arpCommand | awk 'BEGIN {print "["} NR>1 \
+						{print "{ \"address\": \"" $1 "\", " \
 									"\"hw_type\": \"" $2 "\", " \
 									"\"hw_address\": \"" $3 "\", " \
 									"\"flags\": \"" $4 "\", " \
 									"\"mask\": \"" $5 "\" }, " \
 									} \
 					END {print "]"}' \
-			| /bin/sed 'N;$s/},/}/;P;D'
+			| /bin/sed 'N;$s/},/}/;P;D')
+
+if [ -z "$result" ];  then echo {}
+else echo $result
+fi 
