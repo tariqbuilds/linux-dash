35dd71b2bd1d84facbb1fa599b194f658d009cd6
Afaq Tariq <afaq05@gmail.com>
2015-02-24 08:51:44 -0500

    closes #290

--- server/modules/shell_files/ram_intensive_processes.sh
+++ server/modules/shell_files/ram_intensive_processes.sh
@@ -3,7 +3,7 @@ result=$(/bin/ps axo pid,user,pmem,rss,vsz,comm --sort -pmem,-rss,-vsz \
 			| head -n 15 \
 			| /usr/bin/awk 'NR>1 {print "{ \"pid\": " $1 \
 										", \"user\": \"" $2 \
-										"\", \"%mem\": " $3 \
+										"\", \"mem_per\": " $3 \
 										", \"rss\": " $4 \
 										", \"vsz\": " $5 \
 										", \"command\": \"" $6 \
