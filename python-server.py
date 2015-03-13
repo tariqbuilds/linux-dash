a17d68df7bc82dc999ebe7888cc49a76be0b056f
Alexander Lozovskoy <alozovskoy@rtlabs.ru>
2015-03-13 12:46:20 +0300

    Add threading

--- python-server.py
+++ python-server.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 
 import os
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer, test as _test
 import subprocess
+from SocketServer import ThreadingMixIn
 
+modulesSubPath = '/server/modules/shell_files/'
+serverPath = os.path.dirname(os.path.realpath(__file__))
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
 
 class MainHandler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         try:
+            data = ''
             contentType = 'text/html'
             if self.path.startswith("/server/"):
-                output = subprocess.Popen(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'server/modules/shell_files/' + self.path.split(
-                    '=')[1] + '.sh', shell=True, stdout=subprocess.PIPE)
+                module = self.path.split('=')[1]
+                output = subprocess.Popen(
+                    serverPath + modulesSubPath + module + '.sh',
+                    shell = True,
+                    stdout = subprocess.PIPE)
                 data = output.communicate()[0]
             else:
                 if self.path == '/':
@@ -31,7 +40,6 @@ class MainHandler(BaseHTTPRequestHandler):
             self.send_error(404, 'File Not Found: %s' % self.path)
 
 if __name__ == '__main__':
-    from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 8081), MainHandler)
+    server = ThreadedHTTPServer(('localhost', 8081), MainHandler)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
