0c5a3e66a79c8160427056cb14d0dfb8617f682f
Afaq Tariq <afaq05@gmail.com>
2015-04-11 00:28:38 -0400

    fixes #300. Now using dig rather than external service

--- server/modules/shell_files/ip_addresses.sh
+++ server/modules/shell_files/ip_addresses.sh
@@ -3,12 +3,12 @@ grepCmd=`which grep`
 sedCmd=`which sed`
 ifconfigCmd=`which ifconfig`
 trCmd=`which tr`
-wgetCmd=`which wget`
+digCmd=`which dig`
 
-externalIp=`$wgetCmd -qO- curlmyip.com`
+externalIp=`$digCmd +short myip.opendns.com @resolver1.opendns.com`
 
 $ifconfigCmd \
 | $grepCmd -B1 "inet addr" \
 | $awkCmd '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' \
-| $awkCmd -F: 'BEGIN {print "["} { print "{ \"interface\": \"" $1 "\", \"ip\": \"" $3 "\" },"} END {print "{ \"interface\": \"external\", \"ip\": \""'$externalIp'"\" } ]"}' \
+| $awkCmd -v exIp="$externalIp" -F: 'BEGIN {print "["} { print "{ \"interface\": \"" $1 "\", \"ip\": \"" $3 "\" },"} END {print "{ \"interface\": \"external\", \"ip\": \""exIp"\" } ]"}' \
 | $trCmd -d '\r\n' 
