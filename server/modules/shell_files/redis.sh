f5619c18783b6c912ad66671c9a56dbbdc6e5123
Afaq Tariq <afaq05@gmail.com>
2015-05-02 17:34:37 -0400

    closes #297

--- server/modules/shell_files/redis.sh
+++ server/modules/shell_files/redis.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-result=$(redis-cli INFO \
+
+########### Enter Your Redis Password  HERE #########
+redisPassword=''
+########### Enter Your Redis Password  HERE #########
+
+redisCommand=$(which redis-cli);
+
+if [ -n "$redisPassword" ]; then
+	redisCommand="$redisCommand -a $redisPassword"    
+fi
+
+result=$($redisCommand INFO \
 			| grep 'redis_version\|connected_clients\|connected_slaves\|used_memory_human\|total_connections_received\|total_commands_processed' \
 			| awk -F: '{print "\"" $1 "\":" "\"" $2 }' \
 			| tr '\r' '"' | tr '\n' ','
