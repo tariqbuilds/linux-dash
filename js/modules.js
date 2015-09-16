836fb1056c6b233577c3e1ccdebdf6aa786ecf13
Afaq Tariq <afaq05@gmail.com>
2015-09-15 20:43:50 -0500

    fixes #325

--- js/modules.js
+++ js/modules.js
@@ -81,13 +81,38 @@ linuxDash.directive('ramChart', ['server', function(server) {
     };
 }]);
 
-linuxDash.directive('cpuLoadChart', ['server', function(server) {
+linuxDash.directive('cpuAvgLoadChart', ['server', function(server) {
     return {
         restrict: 'E',
         scope: {},
         templateUrl: 'templates/modules/cpu-load.html',
         link: function (scope) {
             scope.units = '%';
+        }
+    };
+}]);
+
+linuxDash.directive('cpuUtilizationChart', ['server', function(server) {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'templates/modules/cpu-utilization-chart.html',
+        link: function (scope) {
+            scope.min = 0;
+            scope.max = 100;
+
+            scope.displayValue = function (serverResponseData) {
+                return serverResponseData;
+            };
+
+            scope.utilMetrics = [
+                {
+                    name: 'Usage',
+                    generate: function (serverResponseData) {
+                        return serverResponseData + ' %'
+                    }
+                },
+            ];
         }
     };
 }]);
