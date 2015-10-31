0a0d963b5f9ccc12d512141e93348d701fff73c4
Afaq Tariq <afaq05@gmail.com>
2015-10-30 21:22:19 -0500

    fixed font issue for numbers and long text. more readable now. also reduced table header sizes with abbrs.

--- server/modules/shell_files/cpu_intensive_processes.sh
+++ server/modules/shell_files/cpu_intensive_processes.sh
@@ -4,10 +4,10 @@ result=$(/bin/ps axo pid,user,pcpu,rss,vsz,comm --sort -pcpu,-rss,-vsz \
 			| head -n 15 \
 			| /usr/bin/awk 'BEGIN{OFS=":"} NR>1 {print "{ \"pid\": " $1 \
 							", \"user\": \"" $2 "\"" \
-							", \"cpu_percent\": " $3 \
+							", \"cpu%\": " $3 \
 							", \"rss\": " $4 \
 							", \"vsz\": " $5 \
-							", \"command\": \"" $6 "\"" "},"\
+							", \"cmd\": \"" $6 "\"" "},"\
 						}')
 
 echo "[" ${result%?} "]"
