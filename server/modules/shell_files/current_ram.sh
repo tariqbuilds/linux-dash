eb8b8b8e12d82551510ad2ff113aa32ac4bf4c61
Afaq Tariq <afaq05@gmail.com>
2015-11-02 22:01:19 -0600

    updated accidentally committed file. dangerous.

--- server/modules/shell_files/current_ram.sh
+++ server/modules/shell_files/current_ram.sh
@@ -4,14 +4,9 @@ freeCmd=`which free`
 awkCmd=`which awk`
 
 procps=$($freeCmd -V | /bin/grep procps-ng)
-procpsVersion=$($freeCmd -V | awk '{ print $4 }')
-
-echo $procpsVersion
 
 if [ -z "$procps" ]; then
 	$freeCmd -tmo | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3-$6-$7 ", \"free\": " $4+$6+$7 " }"}'
-elif [[ "$procpsVersion" == "3.3.9"* ]]; then
-	echo "yo homie";
 else
 	$freeCmd -tm | $awkCmd 'NR==2 {print "{ \"total\": " $2 ", \"used\": " $3-$6-$7 ", \"free\": " $4+$6+$7 " }"}'
 fi
