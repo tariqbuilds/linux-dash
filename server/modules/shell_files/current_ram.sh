eb571a6da5a132e9c154fbc650cbb1418adf8135
Afaq Tariq <afaq05@gmail.com>
2015-11-08 00:53:02 -0600

    updated current ram unit issue when parsing proc/meminfo

--- server/modules/shell_files/current_ram.sh
+++ server/modules/shell_files/current_ram.sh
@@ -11,4 +11,4 @@ memInfoFile="/proc/meminfo"
 
 memInfo=`$catCmd $memInfoFile | $grepCmd 'MemTotal\|MemFree\|Buffers\|Cached'`
 
-echo $memInfo | $awkCmd '{print "{ \"total\": " ($2/1024) ", \"used\": " ( ($2-($5+$8+$11))/1024 ) ", \"free\": " ($5+$8+$11) " }"  }'
+echo $memInfo | $awkCmd '{print "{ \"total\": " ($2/1024) ", \"used\": " ( ($2-($5+$8+$11))/1024 ) ", \"free\": " (($5+$8+$11)/1024) " }"  }'
