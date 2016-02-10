9f331cbe26fa565fae196ef5075ef61f0b6ab5d8
Afaq Tariq <afaq05@gmail.com>
2016-02-10 00:50:19 -0500

    fixes #350 . Thanks, @j75

--- server/modules/shell_files/load_avg.sh
+++ server/modules/shell_files/load_avg.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-numberOfCores=$(/bin/grep -c 'model name' /proc/cpuinfo)
+
+grepCmd=`which grep`
+awkCmd=`which awk`
+catCmd=`which cat`
+
+numberOfCores=$($grepCmd -c 'processor' /proc/cpuinfo)
 
 if [ $numberOfCores -eq 0 ]; then
 	numberOfCores=1
 fi
 
-result=$(/bin/cat /proc/loadavg | /usr/bin/awk '{print "{ \"1_min_avg\": " ($1*100)/'$numberOfCores' ", \"5_min_avg\": " ($2*100)/'$numberOfCores' ", \"15_min_avg\": " ($3*100)/'$numberOfCores' "}," }')
+result=$($catCmd /proc/loadavg | $awkCmd '{print "{ \"1_min_avg\": " ($1*100)/'$numberOfCores' ", \"5_min_avg\": " ($2*100)/'$numberOfCores' ", \"15_min_avg\": " ($3*100)/'$numberOfCores' "}," }')
 
 echo ${result%?}
