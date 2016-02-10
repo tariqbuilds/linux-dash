f93af97800f1f6564ffe8856b4d83c9042c5ea09
Afaq Tariq <afaq05@gmail.com>
2016-02-10 00:45:46 -0500

    using which to identify awk path

--- server/modules/shell_files/swap.sh
+++ server/modules/shell_files/swap.sh
@@ -2,13 +2,14 @@
 
 catCmd=`which cat`;
 wcCmd=`which wc`;
+awkCmd=`which awk`
 
 swapLineCount=$($catCmd /proc/swaps | $wcCmd -l)
 
 if [ "$swapLineCount" -gt 1 ]; then
 
   result=$($catCmd /proc/swaps \
-			| /usr/bin/awk 'NR>1 {print "{ \"filename\": \"" $1"\", \"type\": \""$2"\", \"size\": \""$3"\", \"used\": \""$4"\", \"priority\": \""$5"\"}," }'
+			| $awkCmd 'NR>1 {print "{ \"filename\": \"" $1"\", \"type\": \""$2"\", \"size\": \""$3"\", \"used\": \""$4"\", \"priority\": \""$5"\"}," }'
 		)
 
   echo [ ${result%?} ]
