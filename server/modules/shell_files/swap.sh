968ca538d3d48d898934e33ef5acac9d89f1cb0f
Afaq Tariq <afaq05@gmail.com>
2016-02-10 00:43:53 -0500

    fixes #351

--- server/modules/shell_files/swap.sh
+++ server/modules/shell_files/swap.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-result=$(/bin/cat /proc/swaps \
+
+catCmd=`which cat`;
+wcCmd=`which wc`;
+
+swapLineCount=$($catCmd /proc/swaps | $wcCmd -l)
+
+if [ "$swapLineCount" -gt 1 ]; then
+
+  result=$($catCmd /proc/swaps \
 			| /usr/bin/awk 'NR>1 {print "{ \"filename\": \"" $1"\", \"type\": \""$2"\", \"size\": \""$3"\", \"used\": \""$4"\", \"priority\": \""$5"\"}," }'
 		)
-echo [ ${result%?} ]
+
+  echo [ ${result%?} ]
+
+else
+  echo []
+fi
