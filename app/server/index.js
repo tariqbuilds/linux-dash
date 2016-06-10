e580697a42fffa13a3fbfad56f3cf347602dba58
Afaq Tariq <afaq05@gmail.com>
2016-06-09 22:12:24 -0500

    fixed json parsing to account for backslashes; removed commas; using string rather than unnecessary string to json to string conversions

--- app/server/index.js
+++ app/server/index.js
@@ -1,107 +1,104 @@
-var express = require('express');
-var app     = require('express')();
-var server  = require('http').Server(app);
-var path    = require('path');
-var spawn   = require('child_process').spawn;
-var fs      = require('fs');
-var ws      = require('websocket').server;
+var express = require('express')
+var app     = require('express')()
+var server  = require('http').Server(app)
+var path    = require('path')
+var spawn   = require('child_process').spawn
+var fs      = require('fs')
+var ws      = require('websocket').server
 
-server.listen(80);
-console.log('Linux Dash Server Started!');
+server.listen(80)
+console.log('Linux Dash Server Started!')
 
-app.use(express.static(path.resolve(__dirname + '/../')));
+app.use(express.static(path.resolve(__dirname + '/../')))
 
 app.get('/', function (req, res) {
-	res.sendFile(path.resolve(__dirname + '/../index.html'));
-});
+	res.sendFile(path.resolve(__dirname + '/../index.html'))
+})
 
 app.get('/websocket', function (req, res) {
 
   res.send({
     websocket_support: true,
-  });
+  })
 
-});
+})
 
 wsServer = new ws({
 	httpServer: server
-});
+})
 
 function getShellFilePath(moduleName) {
-  return __dirname + '/linux_json_api.sh';
+  return __dirname + '/linux_json_api.sh'
 }
 
 function shellPathAndModuleNameAreValid(shellFilePath, moduleName) {
 
-  var moduleInvalidName = moduleName.indexOf('.') > -1;
-  var moduleNameEmpty   = !moduleName;
-  var moduleNotFound    = !fs.existsSync(shellFilePath);
-  var isValid = true;
+  var moduleInvalidName = moduleName.indexOf('.') > -1
+  var moduleNameEmpty   = !moduleName
+  var moduleNotFound    = !fs.existsSync(shellFilePath)
+  var isValid = true
 
   if (moduleInvalidName || moduleNameEmpty || moduleNotFound) {
-    isValid = false;
+    isValid = false
   }
 
-  return isValid;
+  return isValid
 }
 
 wsServer.on('request', function(request) {
 
-	var wsClient = request.accept('', request.origin);
+	var wsClient = request.accept('', request.origin)
 
   wsClient.on('message', function(wsReq) {
 
-    var moduleName = wsReq.utf8Data;
-    var shellFile  = getShellFilePath(moduleName);
+    var moduleName = wsReq.utf8Data
+    var shellFile  = getShellFilePath(moduleName)
 
     if (!shellPathAndModuleNameAreValid(shellFile, moduleName)) {
-      return;
+      return
     }
 
-		var command = spawn(shellFile, [ moduleName, wsReq.color || '' ]);
-		var output  = [];
+		var command = spawn(shellFile, [ moduleName, wsReq.color || '' ])
+		var output  = []
 
 		command.stdout.on('data', function(chunk) {
-			output.push(chunk);
-		});
+      if (moduleName === 'memory_info') console.log(chunk.toString())
+			output.push(chunk.toString())
+		})
 
 		command.on('close', function(code) {
 
 			if (code === 0) {
 
-        var wsResponse = {
-          moduleName: moduleName,
-				  output: output.toString(),
-        };
-
-				wsClient.sendUTF(JSON.stringify(wsResponse));
+        var wsResponse = '{ "moduleName": "' + moduleName + '", "output": "'+ output.join('') +'" }'
+				wsClient.sendUTF(wsResponse)
 			}
 
-		});
+		})
 
-  });
+  })
 
-});
+})
 
 app.get('/server/', function (req, res) {
 
-	var shellFile = getShellFilePath(req.query.module);
+	var shellFile = getShellFilePath(req.query.module)
 
 	if (!shellPathAndModuleNameAreValid(shellFile, req.query.module)) {
-    res.sendStatus(406);
-    return;
+    res.sendStatus(406)
+    return
   }
 
-	var command = spawn(shellFile, [ req.query.color || '' ]);
-	var output  = [];
+	var command = spawn(shellFile, [ req.query.color || '' ])
+	var output  = []
 
 	command.stdout.on('data', function(chunk) {
-		output.push(chunk);
-	});
+		output.push(chunk)
+	})
 
 	command.on('close', function(code) {
-		if (code === 0) res.send(output.toString());
-		else res.sendStatus(500);
-	});
+		if (code === 0) res.send(output.toString())
+		else res.sendStatus(500)
+	})
 
-});
+})
