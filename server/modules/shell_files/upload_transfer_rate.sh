3bef19f30e409ed6b1bc71d6ad9b38638d0f2cce
Raphael Freudiger <laser_b@gmx.ch>
2016-07-06 07:46:03 +0200

    speed up transfer rate script
    The script does the following:
    1. Read first values for all interfaces and store them in an array
    2. Sleep once. I even decreased the time to half a second.
    3. Get second values and do the calculation.

--- server/modules/shell_files/upload_transfer_rate.sh
+++ server/modules/shell_files/upload_transfer_rate.sh
@@ -3,27 +3,38 @@
 files=(/sys/class/net/*)
 pos=$(( ${#files[*]} - 1 ))
 last=${files[$pos]}
+out1=()
 
 json_output="{"
 
-for interface in "${files[@]}" 
+for interface in "${files[@]}"
 do
 	basename=$(basename "$interface")
-	
-	# find the number of bytes transfered for this interface
-	out1=$(cat /sys/class/net/"$basename"/statistics/tx_bytes)
 
-	# wait a second
-	sleep 1
+	# find the number of bytes transfered for this interface
+	out1+=( $(cat /sys/class/net/"$basename"/statistics/tx_bytes) )
+done
+
+# wait a moment
+sleep 0.5
+
+for interface in "${files[@]}"
+do
+	basename=$(basename "$interface")
 
 	# check same interface again
-	out2=$(cat /sys/class/net/"$basename"/statistics/tx_bytes)	
+	out2=$(cat /sys/class/net/"$basename"/statistics/tx_bytes)
+
+	# read and remove first element
+	out=${out1[0]}
+	unset out1[0]
+	out1=( ${out1[@]} )
 
 	# get the difference (transfer rate)
-	out_bytes=$((out2 - out1))
+	out_bytes=$((out2 - out))
 
 	# convert transfer rate to KB
-	out_kbytes=$((out_bytes / 1024))
+	out_kbytes=$((out_bytes / 1024 * 2))
 
 	# convert transfer rate to KB
 	json_output="$json_output \"$basename\": $out_kbytes"
@@ -33,7 +44,7 @@ do
 	then
 		# add a comma to the line (JSON formatting)
 		json_output="$json_output,"
-	fi 
+	fi
 done
 
 # close the JSON object & print to screen
