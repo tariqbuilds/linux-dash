ad0c797164d716b8ff5e90412d323727b692212e
Afaq Tariq <afaq05@gmail.com>
2017-05-13 17:38:44 -0500

    addressed another inexplicable issue with quotations not escaping. something is afoot

--- app/server/linux_json_api.sh
+++ app/server/linux_json_api.sh
@@ -431,12 +431,7 @@ ram_intensive_processes() {
 recent_account_logins() {
 
   result=$(/usr/bin/lastlog -t 365 \
-        | /usr/bin/awk 'NR>1 {\
-          print "{ \
-            'user': '" $1 "', \
-            'ip': '" $3 "',"" \
-            'date': '" $5" "$6" "$7" "$8" "$9 "'},"
-          }'
+        | /usr/bin/awk 'NR>1 { print "{ \"user\": \"" $1 "\", " "\"ip\": \"" $3 "\"," "\"date\": \""  $5" "$6" "$7" "$8" "$9  "\" }" }'
       )
   echo [ ${result%?} ] | _parseAndPrint
 }
