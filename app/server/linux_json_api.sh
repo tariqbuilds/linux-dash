1489fe574de53b898423dee29120e126fbb89fde
Afaq Tariq <afaq05@gmail.com>
2017-05-13 17:48:23 -0500

    quotation issue with common_applications func

--- app/server/linux_json_api.sh
+++ app/server/linux_json_api.sh
@@ -36,12 +36,7 @@ bandwidth() {
 common_applications() {
   result=$(whereis php node mysql mongo vim python ruby java apache2 nginx openssl vsftpd make \
   | awk -F: '{if(length($2)==0) { installed="false"; } else { installed="true"; } \
-        print \
-        "{ \
-          'binary': '"$1"', \
-          'location': '"$2"', \
-          'installed': "installed" \
-        },"}')
+        print "{ \"binary\": \"" $1 "\", \"location\": \"" $2 "\", \"installed\": \"installed\" },"}')
 
   echo "[" ${result%?} "]" | _parseAndPrint
 }
