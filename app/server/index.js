1afb321af771330e06aa19d217646dcf16c52cef
Artemix <artemix@artemix.org>
2018-05-24 18:54:34 +0200

    Added customizable host support

--- app/server/index.js
+++ app/server/index.js
@@ -6,10 +6,11 @@ var spawn   = require('child_process').spawn
 var fs      = require('fs')
 var ws      = require('websocket').server
 var args    = require('yargs').argv
+var host    = args.host || process.env.LINUX_DASH_SERVER_HOST || '0.0.0.0'
 var port    = args.port || process.env.LINUX_DASH_SERVER_PORT || 80
 
-server.listen(port, function() {
-  console.log('Linux Dash Server Started on port ' + port + '!');
+server.listen(port, host, function() {
+  console.log('Linux Dash Server Started on ' + host + ':' + port + '!');
 })
 
 app.use(express.static(path.resolve(__dirname + '/../')))
