859db2cd99c414023628f734e16f68de88c74825
周经纬 <jingwei.zhou@eoitek.com>
2020-02-18 11:38:56 +0800

    sub "\" and " " to response to web

--- app/server/index.py
+++ app/server/index.py
@@ -31,6 +31,7 @@ class MainHandler(BaseHTTPRequestHandler):
                     shell = True,
                     stdout = subprocess.PIPE)
                 data = output.communicate()[0]
+                data = re.sub(r"[(\\)( )]","",data)
             else:
                 if self.path == '/':
                     self.path = 'index.html'
